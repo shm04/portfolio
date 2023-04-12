77a2efc06630fae7b2533ed30e9562f86f3d9dbc
Asad Shareef <102785064+asadmisrani@users.noreply.github.com>
2023-04-13 01:03:12 +0500

    Update Script.js file
    Modification in Script.js file.

--- script.js
+++ script.js
@@ -117,3 +117,18 @@ seeBtn.forEach((seeBtn) => {
     // Please paste here
   });
 });
+card.forEach((card) => {
+      card.appendChild(popup);
+    });
+    const closeBtn = document.querySelector('.close-button');
+    card.forEach((card) => {
+      closeBtn.addEventListener('click', () => {
+        card.removeChild(popup);
+      });
+    });
+    const closeBtnDesktop = document.querySelector('.close-button-desktop');
+    card.forEach((card) => {
+      closeBtnDesktop.addEventListener('click', () => {
+        card.removeChild(popup);
+      });
+    });
